ef9e06617a3b230d4741226a3e4ad8b4d5885bb3
essohanam <essohanam.alou@imsp-uac.org>
2022-12-07 21:08:44 +0000

    replace Promises using async/await

--- src/index.js
+++ src/index.js
@@ -2,45 +2,23 @@ import style from './style.css';
 import html from './index.html';
 
 const imageTag = document.querySelector('img');
-const cropRequestUrl = 'https://api.giphy.com/v1/gifs/translate?api_key=SsSqGDRjyoH2TXKUZJ98g5EeFuLZecnk&s='
+const cropRequestUrl = 'https://api.giphy.com/v1/gifs/translate?api_key=SsSqGDRjyoH2TXKUZJ98g5EeFuLZecnk&s=';
 let searchName = 'cats';
-fetch(
-    cropRequestUrl + searchName
-    , { mode: 'cors' }
-)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (response) {
-        imageTag.src = response.data.images.original.url;
-    })
-
-const myButton = document.querySelector('button');
-myButton.addEventListener('click', changeCatgif);
-
-function changeCatgif() {
-    fetch(
-        cropRequestUrl + searchName
-        , { mode: 'cors' }
-    )
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (response) {
-            imageTag.src = response.data.images.original.url;
-        }).catch(function (err) {
-            console.error(err);
-            searchName = 'cats';
-        })
-
+async function getGifFromGify() {
+    const response = await fetch(cropRequestUrl + searchName, { mode: 'cors' });
+    const responseData = await response.json();
+    imageTag.src = responseData.data.images.original.url;
 }
 
+getGifFromGify();
+const myButton = document.querySelector('button');
+myButton.addEventListener('click', getGifFromGify);
 const searchBar = document.querySelector('.search');
 const searchIconContainer = document.querySelector('span');
 searchIconContainer.addEventListener('click', changeSearch);
 function changeSearch() {
-    if (!(searchBar.value == '')) {
+    if (!(searchBar.value === '')) {
         searchName = `${searchBar.value}`;
-        changeCatgif();
+        getGifFromGify();
     }
 }
