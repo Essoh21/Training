d0b864d332ce840724c212677c8e6be2d01f421c
essohanam <essohanam.alou@imsp-uac.org>
2022-12-07 20:51:31 +0000

    request with Promises

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import style from './style.css';
 import html from './index.html';
 
 const imageTag = document.querySelector('img');
+const cropRequestUrl = 'https://api.giphy.com/v1/gifs/translate?api_key=SsSqGDRjyoH2TXKUZJ98g5EeFuLZecnk&s='
+let searchName = 'cats';
 fetch(
-    'https://api.giphy.com/v1/gifs/translate?api_key=SsSqGDRjyoH2TXKUZJ98g5EeFuLZecnk&s=cats'
+    cropRequestUrl + searchName
     , { mode: 'cors' }
 )
     .then(function (response) {
@@ -12,4 +14,33 @@ fetch(
     .then(function (response) {
         imageTag.src = response.data.images.original.url;
     })
-//setInterval(() => { window.location.reload() }, 10000);
+
+const myButton = document.querySelector('button');
+myButton.addEventListener('click', changeCatgif);
+
+function changeCatgif() {
+    fetch(
+        cropRequestUrl + searchName
+        , { mode: 'cors' }
+    )
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            imageTag.src = response.data.images.original.url;
+        }).catch(function (err) {
+            console.error(err);
+            searchName = 'cats';
+        })
+
+}
+
+const searchBar = document.querySelector('.search');
+const searchIconContainer = document.querySelector('span');
+searchIconContainer.addEventListener('click', changeSearch);
+function changeSearch() {
+    if (!(searchBar.value == '')) {
+        searchName = `${searchBar.value}`;
+        changeCatgif();
+    }
+}
